7f936f7171b3ce904a9942a527903d81f91f1481
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-07-24 08:18:22 -0300

    INITIAL COMMIT

---
+++ springsecurity/src/test/java/com/simplifiedx/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifiedx.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
